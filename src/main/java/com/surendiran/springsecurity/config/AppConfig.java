1f250f2d1518e375188c7aec76c57251f4b83f8b
Surendiran S <surenyas1902@gmail.com>
2021-10-15 20:02:01 +0530

    In Memory authentication added

---
+++ src/main/java/com/surendiran/springsecurity/config/AppConfig.java
@@ -0,0 +1,28 @@
+package com.surendiran.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.surendiran.springsecurity")
+public class AppConfig {
+
+	// Define a view for the ViewResolver
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+		
+	}
+}
---
+++ src/main/java/com/surendiran/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.surendiran.springsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+			.withUser(users.username("surendiran").password("test123").roles("EMPLOYEE"))
+			.withUser(users.username("yaswanth").password("test123").roles("MANAGER"))
+			.withUser(users.username("somnath").password("test123").roles("ADMIN"));
+		
+	}
+
+}
